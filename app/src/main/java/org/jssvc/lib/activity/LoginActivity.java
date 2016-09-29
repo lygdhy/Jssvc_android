ad8761240e49ba200a247ba14797887f2ad2c54b
HYdong <window2004@126.com>
2016-09-29 08:04:19 +0800

    改成登陆

---
+++ app/src/main/java/org/jssvc/lib/activity/LoginActivity.java
@@ -0,0 +1,32 @@
+package org.jssvc.lib.activity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.jssvc.lib.R;
+import org.jssvc.lib.base.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * 用户登录
+ */
+public class LoginActivity extends BaseActivity {
+
+    @BindView(R.id.tvBack)
+    TextView tvBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.tvBack)
+    public void onClick() {
+        finish();
+    }
+}
