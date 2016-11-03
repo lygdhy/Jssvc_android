13204c65f9a1a649a80302c92116c4a32a4f0266
HYdong <window2004@126.com>
2016-11-03 20:28:45 +0800

    QQ客服扩展

--- app/src/main/java/org/jssvc/lib/activity/SettingActivity.java
+++ app/src/main/java/org/jssvc/lib/activity/SettingActivity.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 import org.jssvc.lib.R;
 import org.jssvc.lib.base.BaseActivity;
 import org.jssvc.lib.data.AccountPref;
+import org.jssvc.lib.utils.AppUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -62,8 +63,23 @@ public class SettingActivity extends BaseActivity {
                 break;
             case R.id.tvFeedback:
                 // 意见反馈
-                String url = "mqqwpa://im/chat?chat_type=wpa&uin=149553453";
-                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                // 手机QQ com.tencent.mqq
+                // 手机QQ2012 com.tencent.mobileqq
+                // QQ轻聊版 com.tencent.qqlite
+                // QQ国际版 com.tencent.mobileqqi
+                // QQHD com.tencent.minihd.qq
+                // 企业QQ com.tencent.eim
+                if (AppUtils.isInstallApp(context, "com.tencent.mqq") ||
+                        AppUtils.isInstallApp(context, "com.tencent.mobileqq") ||
+                        AppUtils.isInstallApp(context, "com.tencent.qqlite") ||
+                        AppUtils.isInstallApp(context, "com.tencent.mobileqqi") ||
+                        AppUtils.isInstallApp(context, "com.tencent.minihd.qq") ||
+                        AppUtils.isInstallApp(context, "com.tencent.eim")) {
+                    String url = "mqqwpa://im/chat?chat_type=wpa&uin=149553453";
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                } else {
+                    showToast("暂未安装QQ相关软件无法发起聊天");
+                }
                 break;
             case R.id.tvAbout:
                 // 关于我们
