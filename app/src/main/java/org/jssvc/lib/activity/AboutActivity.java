d28e4b3abab838edd64188ad002d8b384cafe83b
dhy <window2004@126.com>
2016-11-08 17:47:54 +0800

    研究S嵌套V

--- app/src/main/java/org/jssvc/lib/activity/AboutActivity.java
+++ app/src/main/java/org/jssvc/lib/activity/AboutActivity.java
@@ -1,10 +1,19 @@
 package org.jssvc.lib.activity;
 
 import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.jssvc.lib.R;
 import org.jssvc.lib.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,12 +26,74 @@ public class AboutActivity extends BaseActivity {
 
     @BindView(R.id.tvBack)
     TextView tvBack;
+    @BindView(R.id.viewPager)
+    ViewPager viewPager;
+
+    View view1, view2, view3;
+    List<View> viewList;//view数组
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         ButterKnife.bind(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        view1 = inflater.inflate(R.layout.pager_layout_1, null);
+        view2 = inflater.inflate(R.layout.pager_layout_2, null);
+        view3 = inflater.inflate(R.layout.pager_layout_3, null);
+
+        view1.getHeight();
+
+        viewList = new ArrayList<View>();// 将要分页显示的View装入数组中
+        viewList.add(view1);
+        viewList.add(view2);
+        viewList.add(view3);
+
+        PagerAdapter pagerAdapter = new PagerAdapter() {
+
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                // TODO Auto-generated method stub
+                return arg0 == arg1;
+            }
+
+            @Override
+            public int getCount() {
+                // TODO Auto-generated method stub
+                return viewList.size();
+            }
+
+            @Override
+            public void destroyItem(ViewGroup container, int position, Object object) {
+                // TODO Auto-generated method stub
+                container.removeView(viewList.get(position));
+            }
+
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                // TODO Auto-generated method stub
+                container.addView(viewList.get(position));
+
+                return viewList.get(position);
+            }
+        };
+
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        // 自定义类似于ViewPager的可上下滑动切换效果的视图
+        // http://blog.csdn.net/u010214991/article/details/50786595
+
+        int ss = view1.getHeight() + view2.getHeight() + view3.getHeight();
+        showToast(ss+"");
+        LinearLayout.LayoutParams linearParams = (LinearLayout.LayoutParams) viewPager.getLayoutParams();
+        linearParams.height = ss;
+        viewPager.setLayoutParams(linearParams);
     }
 
     @OnClick(R.id.tvBack)
