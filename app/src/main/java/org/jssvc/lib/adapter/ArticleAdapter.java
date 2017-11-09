df4d6f534b5ad381f29dfbf00a06c6a140e3e689
dhy <window2004@126.com>
2017-11-09 10:20:42 +0800

    图片为空是不显示

--- app/src/main/java/org/jssvc/lib/adapter/ArticleAdapter.java
+++ app/src/main/java/org/jssvc/lib/adapter/ArticleAdapter.java
@@ -2,6 +2,9 @@ package org.jssvc.lib.adapter;
 
 import android.support.v7.widget.RecyclerView;
 
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.ImageView;
 import org.jssvc.lib.R;
 import org.jssvc.lib.bean.ArticleListBean;
 import org.jssvc.lib.utils.ImageLoader;
@@ -21,7 +24,13 @@ public class ArticleAdapter extends BGARecyclerViewAdapter<ArticleListBean> {
 
   @Override
   protected void fillData(BGAViewHolderHelper helper, int position, ArticleListBean model) {
-    ImageLoader.with(mContext, helper.getImageView(R.id.msgCover), model.getBanner());
+    ImageView banner = helper.getImageView(R.id.msgCover);
+    if (TextUtils.isEmpty(model.getBanner())) {
+      banner.setVisibility(View.GONE);
+    } else {
+      banner.setVisibility(View.VISIBLE);
+      ImageLoader.with(mContext, banner, model.getBanner());
+    }
 
     helper.getTextView(R.id.tvTitle).setText(model.getTitle() + "");
     helper.getTextView(R.id.tvAuthor).setText(model.getAuthor() + "");
