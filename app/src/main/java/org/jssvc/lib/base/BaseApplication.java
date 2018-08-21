c2d4198a5db52227beabdf861129270512653582
七忆鱼 <451658133@qq.com>
2018-08-21 17:39:43 +0800

    打包2.1.12

--- app/src/main/java/org/jssvc/lib/base/BaseApplication.java
+++ app/src/main/java/org/jssvc/lib/base/BaseApplication.java
@@ -3,6 +3,7 @@ package org.jssvc.lib.base;
 import android.app.Activity;
 import android.support.multidex.MultiDexApplication;
 import android.view.View;
+
 import com.blankj.utilcode.util.SPUtils;
 import com.blankj.utilcode.util.Utils;
 import com.lzy.okgo.OkGo;
@@ -15,11 +16,14 @@ import com.mob.MobSDK;
 import com.pgyersdk.crash.PgyCrashManager;
 import com.tencent.smtt.sdk.QbSdk;
 import com.umeng.analytics.MobclickAgent;
-import java.util.concurrent.TimeUnit;
-import java.util.logging.Level;
-import okhttp3.OkHttpClient;
+
 import org.jssvc.lib.bean.UserBean;
 import org.jssvc.lib.view.ActivityLifecycleHelper;
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import okhttp3.OkHttpClient;
 
 /**
  * Created by lygdh on 2016/11/15.
@@ -27,85 +31,86 @@ import org.jssvc.lib.view.ActivityLifecycleHelper;
 
 public class BaseApplication extends MultiDexApplication {
 
-  public static SPUtils spUtils;
-  private static final String FILE_NAME = "sp_jssvc_lib";
+    public static SPUtils spUtils;
+    private static final String FILE_NAME = "sp_jssvc_lib";
 
-  BaseApplication appContext;
-  public static UserBean localUserBean = null;// 全局账户（来自SharedPreferences）
-  public static boolean libOnline = false;//图书馆在线（来自内存）
+    BaseApplication appContext;
+    public static UserBean localUserBean = null;// 全局账户（来自SharedPreferences）
+    public static boolean libOnline = false;//图书馆在线（来自内存）
 
-  private ActivityLifecycleHelper mActivityLifecycleHelper;
+    private ActivityLifecycleHelper mActivityLifecycleHelper;
 
-  @Override public void onCreate() {
-    super.onCreate();
-    appContext = this;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        appContext = this;
 
-    Utils.init(appContext);
-    if (spUtils == null) {
-      spUtils = SPUtils.getInstance(FILE_NAME);
+        Utils.init(appContext);
+        if (spUtils == null) {
+            spUtils = SPUtils.getInstance(FILE_NAME);
+        }
+
+        // 蒲公英
+        PgyCrashManager.register(this);
+
+        // 友盟错误统计
+        MobclickAgent.setCatchUncaughtExceptions(true);
+
+        // 初始化OkGo
+        initOkGo();
+
+        // Mob SDK
+        //SMSSDK.initSDK(this, "1e85bf08b2f08", "7d2ed9146830fca64601549ca9f87c93");
+        MobSDK.init(this, null, null);
+
+        // X5浏览服务
+        QbSdk.initX5Environment(appContext, null);
+
+        // 注册滑动返回
+        registerActivityLifecycleCallbacks(mActivityLifecycleHelper = new ActivityLifecycleHelper());
     }
-
-    // 蒲公英
-    PgyCrashManager.register(this);
-
-    // 友盟错误统计
-    MobclickAgent.setCatchUncaughtExceptions(true);
 
     // 初始化OkGo
-    initOkGo();
+    private void initOkGo() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        // 配置log
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkGo");
+        loggingInterceptor.setPrintLevel(HttpLoggingInterceptor.Level.BODY);
+        loggingInterceptor.setColorLevel(Level.INFO);
+        builder.addInterceptor(loggingInterceptor);
+        //全局的读取超时时间
+        builder.readTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
+        //全局的写入超时时间
+        builder.writeTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
+        //全局的连接超时时间
+        builder.connectTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
 
-    // Mob SDK
-    //SMSSDK.initSDK(this, "1e85bf08b2f08", "7d2ed9146830fca64601549ca9f87c93");
-    MobSDK.init(this, null, null);
+        //使用数据库保持cookie，如果cookie不过期，则一直有效
+        builder.cookieJar(new CookieJarImpl(new DBCookieStore(this)));
 
-    // X5浏览服务
-    QbSdk.initX5Environment(appContext, null);
-
-    // 注册滑动返回
-    registerActivityLifecycleCallbacks(mActivityLifecycleHelper = new ActivityLifecycleHelper());
-  }
-
-  // 初始化OkGo
-  private void initOkGo() {
-    OkHttpClient.Builder builder = new OkHttpClient.Builder();
-    // 配置log
-    HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkGo");
-    loggingInterceptor.setPrintLevel(HttpLoggingInterceptor.Level.BODY);
-    loggingInterceptor.setColorLevel(Level.INFO);
-    builder.addInterceptor(loggingInterceptor);
-    //全局的读取超时时间
-    builder.readTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
-    //全局的写入超时时间
-    builder.writeTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
-    //全局的连接超时时间
-    builder.connectTimeout(OkGo.DEFAULT_MILLISECONDS, TimeUnit.MILLISECONDS);
-
-    //使用数据库保持cookie，如果cookie不过期，则一直有效
-    builder.cookieJar(new CookieJarImpl(new DBCookieStore(this)));
-
-    OkGo.getInstance().init(this)                       //必须调用初始化
-        .setOkHttpClient(builder.build())               //设置OkHttpClient
-        .setCacheMode(CacheMode.FIRST_CACHE_THEN_REQUEST)//先使用缓存，不管是否存在，仍然请求网络
-        .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)   //全局统一缓存时间，默认永不过期，可以不传
-        .setRetryCount(3);                       //全局公共参数
-  }
-
-  public ActivityLifecycleHelper getActivityLifecycleHelper() {
-    return mActivityLifecycleHelper;
-  }
-
-  public void onSlideBack(boolean isReset, float distance) {
-    if (mActivityLifecycleHelper != null) {
-      Activity lastActivity = mActivityLifecycleHelper.getPreActivity();
-      if (lastActivity != null) {
-        View contentView = lastActivity.findViewById(android.R.id.content);
-        if (isReset) {
-          contentView.setX(contentView.getLeft());
-        } else {
-          final int width = getResources().getDisplayMetrics().widthPixels;
-          contentView.setX(-width / 3 + distance / 3);
-        }
-      }
+        OkGo.getInstance().init(this)                       //必须调用初始化
+                .setOkHttpClient(builder.build())               //设置OkHttpClient
+                .setCacheMode(CacheMode.FIRST_CACHE_THEN_REQUEST)//先使用缓存，不管是否存在，仍然请求网络
+                .setCacheTime(CacheEntity.CACHE_NEVER_EXPIRE)   //全局统一缓存时间，默认永不过期，可以不传
+                .setRetryCount(3);                       //全局公共参数
     }
-  }
+
+    public ActivityLifecycleHelper getActivityLifecycleHelper() {
+        return mActivityLifecycleHelper;
+    }
+
+    public void onSlideBack(boolean isReset, float distance) {
+        if (mActivityLifecycleHelper != null) {
+            Activity lastActivity = mActivityLifecycleHelper.getPreActivity();
+            if (lastActivity != null) {
+                View contentView = lastActivity.findViewById(android.R.id.content);
+                if (isReset) {
+                    contentView.setX(contentView.getLeft());
+                } else {
+                    final int width = getResources().getDisplayMetrics().widthPixels;
+                    contentView.setX(-width / 3 + distance / 3);
+                }
+            }
+        }
+    }
 }
